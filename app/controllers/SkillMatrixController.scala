c90c70f1eab561b9e4aeb8a958777c6f5d5aa696
Tatiana Moldovan <tatiana.moldovan@ing.nl>
2017-02-10 16:44:39 +0100

    Refactor: handle future results

--- app/controllers/SkillMatrixController.scala
+++ app/controllers/SkillMatrixController.scala
@@ -6,7 +6,7 @@ import models.EnumTypes.SkillLevel.SkillLevel
 import models.Skill
 import play.api.libs.json.{JsError, Json}
 import play.api.mvc._
-import services.SkillMatrixService
+import services.SkillMatrixDAOService
 
 import scala.concurrent._
 import ExecutionContext.Implicits.global
@@ -15,7 +15,7 @@ import scala.concurrent.Future
 /**
   * Created by tatianamoldovan on 06/02/2017.
   */
-class SkillMatrixController @Inject()(skillMatrixService: SkillMatrixService) extends Controller {
+class SkillMatrixController @Inject()(skillMatrixDAOService: SkillMatrixDAOService) extends Controller {
 
   def addSkillByUserId(userId: Int) = Action.async(BodyParsers.parse.json) { request =>
     val skill = (request.body \ "skill").validate[Skill]
@@ -26,7 +26,7 @@ class SkillMatrixController @Inject()(skillMatrixService: SkillMatrixService) ex
         "error" -> JsError.toJson(errors)
       ))),
       _ =>
-        skillMatrixService.addSkillByUserId(userId, skill.get, skillLevel.get).map(m =>
+        skillMatrixDAOService.addSkillByUserIdToSkillMatrix(userId, skill.get, skillLevel.get).map(m =>
           Ok(Json.obj("status" -> "Success", "skillAdded" -> Json.toJson(m))))
     )
   }
@@ -40,19 +40,19 @@ class SkillMatrixController @Inject()(skillMatrixService: SkillMatrixService) ex
         "error" -> JsError.toJson(errors)
       ))),
       _ =>
-        skillMatrixService.updateSkillByUserId(userSkillId, userId, skill.get, skillLevel.get).map(_ =>
+        skillMatrixDAOService.updateSkill(userSkillId, userId, skill.get, skillLevel.get).map(_ =>
           Ok(Json.obj("status" -> "Success")))
     )
   }
 
   def deleteSkillByUserId(userId: Int, userSkillId: Int) = Action.async(BodyParsers.parse.empty) { _ =>
-    skillMatrixService.deleteSkillByUserId(userSkillId).map(_ =>
+    skillMatrixDAOService.deleteSkillByUserId(userSkillId).map(_ =>
       Ok(Json.obj("status" -> "Success"))
     )
   }
 
   def getSkillsByUserId(userId: Int) = Action.async(BodyParsers.parse.empty) { _ =>
-    skillMatrixService.getAllSkillsByUserId(userId).map(m =>
+    skillMatrixDAOService.getAllSkillsByUserId(userId).map(m =>
       Ok(Json.obj("status" -> "Success", "skills" -> Json.toJson(m)))
     )
   }
@@ -63,7 +63,7 @@ class SkillMatrixController @Inject()(skillMatrixService: SkillMatrixService) ex
   }*/
 
   def getSkillById(skillId: Int) = Action.async(BodyParsers.parse.empty) { _ =>
-    skillMatrixService.getSkillById(skillId).map(m =>
+    skillMatrixDAOService.getSkillById(skillId).map(m =>
       Ok(Json.obj("status" -> "Success", "skills" -> Json.toJson(m))))
   }
 }
