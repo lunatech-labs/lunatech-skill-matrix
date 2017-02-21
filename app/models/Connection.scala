1f7af8503f53d84521e66f246eed31096e51cf81
Pedro Ferreira <pedro.ferreira@lunatech.com>
2017-02-21 16:32:23 +0100

    Changed to get existing DB connection

--- app/models/Connection.scala
+++ app/models/Connection.scala
@@ -1,9 +1,10 @@
 package models
 
 import slick.driver.JdbcProfile
-import slick.driver.PostgresDriver.api._
 import play.api.db.slick.DatabaseConfigProvider
+import play.api.Play
 
 object Connection{
-  val db = Database.forURL("jdbc:postgresql://localhost:5432/skillmatrix?user=postgres&password=root", driver="org.postgresql.Driver")
+
+  val db = DatabaseConfigProvider.get[JdbcProfile](Play.current).db
 }
