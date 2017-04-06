62affa287180fb133207aa3e4c86fcfa9c17eb44
Pedro Ferreira <pedro.ferreira@lunatech.com>
2017-04-06 09:26:14 +0200

    Changing look and feel, including angular-material

---
+++ public/javascripts/menu/menu.controller.js
@@ -0,0 +1,11 @@
+angular.module('techmatrix').controller('MenuController', [
+  '$scope',
+  '$location',
+  '$mdSidenav',
+  function($scope, $location, $mdSidenav){
+
+    $scope.goTo = function(path){
+      $mdSidenav('menu').toggle();
+      $location.path(path);
+    }
+}]);
