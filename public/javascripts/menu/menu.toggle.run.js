54409f901f3d559a4fa62e78430f409f15f66960
Pedro Ferreira <pedro.ferreira@lunatech.com>
2017-07-25 17:28:02 +0200

    Adding avatar

--- public/javascripts/menu/menu.toggle.run.js
+++ public/javascripts/menu/menu.toggle.run.js
@@ -1,10 +1,15 @@
 (function () {
   'use strict';
 
-  angular.module('techmatrix').run(['$rootScope', '$mdSidenav', function($rootScope, $mdSidenav){
+  angular.module('techmatrix').run(['$rootScope', '$mdSidenav','$cookies', function($rootScope, $mdSidenav, $cookies){
 
     $rootScope.toggleMenu = function(){
       $mdSidenav('menu').toggle();
     }
+    $rootScope.details = {
+      avatar: $cookies.getObject('user') ? $cookies.getObject('user').avatar : '',
+      name: $cookies.getObject('user') ? $cookies.getObject('user').full_name : ''
+    };
+
   }]);
 })()
