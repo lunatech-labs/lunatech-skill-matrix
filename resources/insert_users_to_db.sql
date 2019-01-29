e1bda084333f072fe7725237d251dfb94c1b56e4
dimitrios.charoulis@lunatech.nl <dimitrios.charoulis@lunatech.nl>
2019-01-29 15:06:41 +0100

    Update script that inserts test users. Update Readme.md.

--- resources/insert_users_to_db.sql
+++ resources/insert_users_to_db.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO users VALUES (DEFAULT, 'Tanya', 'Moldovan', 'tanya.moldovan@lunatech.com');
-INSERT INTO users VALUES (DEFAULT, 'Scooby', 'Doo', 'tanya.moldovan@gmail.com');
-INSERT INTO users VALUES (DEFAULT, 'Test', 'Users', 'test@test.com');
-INSERT INTO users VALUES (DEFAULT, 'Severus', 'Snape', 'severus.snape@hogwarts.com')
+﻿INSERT INTO users VALUES (DEFAULT, 'Tanya', 'Moldovan', 'tanya.moldovan@lunatech.com', '{"Developer"}', 'Active');
+INSERT INTO users VALUES (DEFAULT, 'Scooby', 'Doo', 'tanya.moldovan@gmail.com', '{"Developer"}', 'Active');
+INSERT INTO users VALUES (DEFAULT, 'Test', 'Users', 'test@test.com', '{"Developer"}', 'Active');
+INSERT INTO users VALUES (DEFAULT, 'Severus', 'Snape', 'severus.snape@hogwarts.com', '{"Developer"}', 'Active')
